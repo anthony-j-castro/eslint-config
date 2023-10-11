162502a7986cbab6aa8b857eb3797ea8ed645ba5
Anthony Castro <anthony-j-castro@users.noreply.github.com>
2023-10-11 18:32:00 -0400

    feat: Create initial configuration (#1)

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: { node: true },
+  extends: "./index.js",
+  root: true,
+  rules: {
+    "sort-keys": "error",
+  },
+};
---
+++ index.js
@@ -0,0 +1,66 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "import/newline-after-import": "error",
+    "import/no-named-as-default": "off",
+    "import/no-relative-packages": "error",
+    "import/no-unresolved": "error",
+    "import/order": [
+      "error",
+      {
+        alphabetize: { order: "asc" },
+        groups: ["builtin", "external", "internal", "sibling", "index"],
+        "newlines-between": "never",
+      },
+    ],
+    "no-console": "error",
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: [
+          {
+            group: ["../*"],
+            message: "Usage of relative parent imports is not allowed.",
+          },
+        ],
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", next: "return", prev: "*" },
+    ],
+    "prettier/prettier": "error",
+    "react/jsx-no-useless-fragment": "error",
+    "react/jsx-sort-props": ["error", { shorthandLast: true }],
+    "sort-imports": ["error", { ignoreDeclarationSort: true }],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+    react: {
+      version: "detect",
+    },
+  },
+};
