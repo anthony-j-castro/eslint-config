f5f7b463a89d1b0a31689cdc82e82a0f6c429dc7
Anthony Castro <anthony-j-castro@users.noreply.github.com>
2023-10-11 19:27:44 -0400

    chore: Tweak semantic-release settings (#5)

--- release.config.js
+++ release.config.js
@@ -4,8 +4,15 @@ const config = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
       "@semantic-release/git",
       {
+        assets: ["CHANGELOG.md", "package.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
