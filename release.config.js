2453315d57cbb4ab8751dd2f2bc9e07b3d5c42f0
Anthony Castro <anthony618@gmail.com>
2023-10-11 19:14:13 -0400

    Update changelog and package.json

--- release.config.js
+++ release.config.js
@@ -12,6 +12,7 @@ const config = {
     [
       "@semantic-release/git",
       {
+        assets: ["CHANGELOG.md", "package.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
