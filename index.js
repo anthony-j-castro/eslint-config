e4492dba7c5c94ff5dd14fd58d1c5f31cc2eed5c
Anthony Castro <anthony618@gmail.com>
2023-10-11 10:50:44 -0400

    Remove root: true from config

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "prettier"],
-  root: true,
   rules: {
     "@typescript-eslint/no-unused-vars": "error",
     "import/newline-after-import": "error",
