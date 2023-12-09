95ec54ceda858badc4459fbca60c0044620463f3
Anthony Castro <anthony618@gmail.com>
2023-12-09 15:18:26 -0500

    Disable successComment for PRs

--- release.config.js
+++ release.config.js
@@ -18,7 +18,12 @@ const config = {
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
-    "@semantic-release/github",
+    [
+      "@semantic-release/github",
+      {
+        successComment: false,
+      },
+    ],
   ],
 };
 
