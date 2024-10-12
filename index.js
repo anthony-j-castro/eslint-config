b664fa3c7041d0c9be1259f53bcf6990d57e905a
Anthony Castro <anthony618@gmail.com>
2024-10-12 14:41:11 -0400

    Update sort-named-imports

--- index.js
+++ index.js
@@ -86,6 +86,12 @@ module.exports = {
         groups: ["unknown", "shorthand"],
       },
     ],
+    "perfectionist/sort-named-imports": [
+      "error",
+      {
+        groupKind: "values-first",
+      },
+    ],
     "perfectionist/sort-object-types": [
       "error",
       { groupKind: "required-first" },
