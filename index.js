8466f667a7aed6fc25c242b04f023a4e68faccd3
Anthony Castro <anthony-j-castro@users.noreply.github.com>
2024-10-22 19:11:32 -0400

    refactor: Move other settings out of rules file (#266)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { fixupConfigRules } = require("@eslint/compat");
 const { FlatCompat } = require("@eslint/eslintrc");
 const js = require("@eslint/js");
 const stylistic = require("@stylistic/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
 const importPlugin = require("eslint-plugin-import");
 const perfectionist = require("eslint-plugin-perfectionist");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
@@ -25,6 +26,27 @@ module.exports = tseslint.config(
   perfectionist.configs["recommended-natural"],
   stylistic.configs["disable-legacy"],
   eslintPluginPrettierRecommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      parser: tsParser,
+      sourceType: "module",
+    },
+  },
+  {
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+        typescript: true,
+      },
+      react: {
+        version: "detect",
+      },
+    },
+  },
   ...rules,
   {
     files: ["**/*.js"],
